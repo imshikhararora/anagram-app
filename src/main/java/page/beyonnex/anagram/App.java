67ac0b91d9b46a77b4ca92dbd5b96e4393b5c009
Shikhar Arora <hi.shikhararora@gmail.com>
2024-02-13 23:23:58 +0100

    handled edge cases and refactor

--- src/main/java/page/beyonnex/anagram/App.java
+++ src/main/java/page/beyonnex/anagram/App.java
@@ -3,6 +3,7 @@ package page.beyonnex.anagram;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,31 +14,48 @@ public class App {
     private static Map<String, Set<String>> anagramsMap = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println("\nAnagram App");
-        System.out.println("Please select an option: ");
-        System.out.println("1 - Check if two texts are anagrams of each other");
-        System.out.println("2 - Get all the anagrams for a given string");
-        System.out.println("3 - Clear the memory");
-        System.out.println("4 - Exit the program");
-        System.out.print("Enter your choice: ");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-        switch (choice) {
-            case 1:
-                checkAnagrams();
-                break;
-            case 2:
-                getAnagrams();
-                break;
-            case 3:
-                clearMemory();
-                break;
-            case 4:
-                System.out.println("Exiting the program .... ");
-                System.exit(0);
-                return;
-            default:
-                System.out.println("Invalid choice");
+        System.out.println("Welcome to the Anagram App!\n");
+        System.out.println("*************************************************");
+        System.out.println("*       Welcome to the Anagram Explorer         *");
+        System.out.println("*  Unravel the world of words with Beyonnex.io  *");
+        System.out.println("*              and Michael Page!                *");
+        System.out.println("*************************************************");
+
+        while (true) {
+            System.out.println("\nAnagram App");
+            System.out.println("Please select an option: ");
+            System.out.println("1 - Check if two texts are anagrams of each other");
+            System.out.println("2 - Get all the anagrams for a given string");
+            System.out.println("3 - Clear the memory");
+            System.out.println("4 - Exit the program");
+            System.out.print("Enter your choice: ");
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        checkAnagrams();
+                        break;
+                    case 2:
+                        getAnagrams();
+                        break;
+                    case 3:
+                        clearMemory();
+                        break;
+                    case 4:
+                        System.out.println("Thank you for using the Anagram App!");
+                        System.out.println("Hope you liked it!");
+                        System.out.println("Exiting the program .... ");
+                        System.exit(0);
+                        return;
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number from 1 to 4.");
+                scanner.nextLine();
+            }
+
         }
     }
 
@@ -51,6 +69,7 @@ public class App {
         String text = scanner.nextLine();
         if (anagramsMap.containsKey(getSortedChars(text))) {
             Set<String> anagramSet = anagramsMap.get(getSortedChars(text));
+            anagramSet.remove(text);
             System.out.println("The anagrams for the given text are: " + anagramSet);
         } else {
             System.out.println("No anagrams found for the given text");
@@ -74,8 +93,8 @@ public class App {
     }
 
     private static void updateAnagramsMap(String text1, String text2, boolean isAnagram) {
+        String key = getSortedChars(text1);
         if (isAnagram) {
-            String key = getSortedChars(text1);
             Set<String> anagramSet;
             if (anagramsMap.containsKey(key)) {
                 anagramSet = anagramsMap.get(key);
@@ -86,7 +105,6 @@ public class App {
             anagramSet.add(text2);
             anagramsMap.put(key, anagramSet);
         } else {
-            String key = getSortedChars(text1);
             addNonAnagramsToAnagramMap(text1, key);
             key = getSortedChars(text2);
             addNonAnagramsToAnagramMap(text2, key);
@@ -94,15 +112,14 @@ public class App {
     }
 
     private static void addNonAnagramsToAnagramMap(String text, String key) {
+        Set<String> anagramSet;
         if (anagramsMap.containsKey(key)) {
-            Set<String> anagramSet = anagramsMap.get(key);
-            anagramSet.add(text);
-            anagramsMap.put(key, anagramSet);
+            anagramSet = anagramsMap.get(key);
         } else {
-            Set<String> anagramSet = new HashSet<>();
-            anagramSet.add(text);
-            anagramsMap.put(key, anagramSet);
+            anagramSet = new HashSet<>();
         }
+        anagramSet.add(text);
+        anagramsMap.put(key, anagramSet);
     }
 
     private static boolean areAnagrams(String text1, String text2) {
